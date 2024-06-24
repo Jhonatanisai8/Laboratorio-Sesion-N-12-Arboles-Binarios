d9435894c3bb21a20248a65411f01ff626a00f51
jhonatan <ojedasnachezjhonatan@gmail.com>
2024-06-24 12:46:33 -0500

    clase cliente constructor y getters

--- src/main/java/org/JhonatanIsai/Modelo/Cliente.java
+++ src/main/java/org/JhonatanIsai/Modelo/Cliente.java
@@ -6,4 +6,31 @@ public class Cliente {
     private String nombres;
     private String telefono;
 
+    public Cliente(Object[] registro) {
+        this.apellidos = registro[0].toString();
+        this.nombres = registro[1].toString();
+        this.telefono = registro[2].toString();
+    }
+
+    public Object[] getRegistro() {
+        Object[] registro = {apellidos, nombres, telefono};
+        return registro;
+    }
+
+    public String getNombreCompleto() {
+        return apellidos + " " + nombres;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
 }

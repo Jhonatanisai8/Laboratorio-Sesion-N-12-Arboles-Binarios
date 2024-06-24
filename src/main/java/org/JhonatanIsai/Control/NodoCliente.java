c9258220678f3413fda656e5faadea895e58e66f
jhonatan <ojedasnachezjhonatan@gmail.com>
2024-06-24 12:49:14 -0500

    clase nodo cliente: constructor y getters

--- src/main/java/org/JhonatanIsai/Control/NodoCliente.java
+++ src/main/java/org/JhonatanIsai/Control/NodoCliente.java
@@ -7,4 +7,22 @@ public class NodoCliente {
     private Cliente elemento;
     private NodoCliente izq, dere;
 
+    public NodoCliente(Cliente elemento) {
+        this.elemento = elemento;
+        izq = null;
+        dere = null;
+    }
+
+    public Cliente getElemento() {
+        return elemento;
+    }
+
+    public NodoCliente getIzq() {
+        return izq;
+    }
+
+    public NodoCliente getDere() {
+        return dere;
+    }
+
 }

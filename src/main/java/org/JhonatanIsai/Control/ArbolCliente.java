a6ca65973c6420183ee3fb326d9929cb840c930f
jhonatan <ojedasnachezjhonatan@gmail.com>
2024-06-24 12:51:46 -0500

    clase arbol-cliente: atributo, constructor, getters y setters

---
+++ src/main/java/org/JhonatanIsai/Control/ArbolCliente.java
@@ -0,0 +1,19 @@
+package org.JhonatanIsai.Control;
+
+public class ArbolCliente {
+
+    private NodoCliente raiz;
+
+    public ArbolCliente() {
+        raiz = null;
+    }
+
+    public NodoCliente getRaiz() {
+        return raiz;
+    }
+
+    public void setRaiz(NodoCliente raiz) {
+        this.raiz = raiz;
+    }
+
+}

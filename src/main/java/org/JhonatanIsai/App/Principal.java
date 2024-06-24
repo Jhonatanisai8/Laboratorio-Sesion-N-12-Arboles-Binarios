ee0bd8ac6ad4a241881dbdef43b4252eac363be2
jhonatan <ojedasnachezjhonatan@gmail.com>
2024-06-24 13:32:20 -0500

    diseño de la interfas

--- src/main/java/org/JhonatanIsai/App/Principal.java
+++ src/main/java/org/JhonatanIsai/App/Principal.java
@@ -1,8 +1,11 @@
 package org.JhonatanIsai.App;
 
+import org.JhonatanIsai.Vista.frmVista;
+
 public class Principal {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        frmVista o = new frmVista();
+        o.setVisible(true);
     }
 }
